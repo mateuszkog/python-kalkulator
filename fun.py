3241fe481f8679289902c2d211785aaad879d3a8
Mateusz Kogut <mateuszkog@gmail.com>
2025-10-02 20:38:35 +0200

    Initial commit

---
+++ fun.py
@@ -0,0 +1,6 @@
+a = input("jak sie nazywasz?")
+
+b = input("Ile masz lat?")
+
+print("Czyli jest 1 do kwadryliona szansy że to jest twój numer karty kredytowej:")
+print("2436 7863 9857 2536")
---
+++ kalkulator.py
@@ -0,0 +1,21 @@
+liczba1 = float(input("podaj 1 liczbę: "))
+liczba2 = float(input("podaj 2 liczbę: "))
+wybierz = int(input("wybierz działanie (1 = +) (2 = -) (3 = *) (4 = /): "))
+
+if wybierz == 1:
+    print(liczba1, "+", liczba2, "=", liczba1 + liczba2)
+
+
+elif wybierz == 2:
+    print(liczba1, "-", liczba2, "=", liczba1 - liczba2)
+
+elif wybierz == 3:
+    print(liczba1, "*", liczba2, "=", liczba1 * liczba2)
+
+
+elif wybierz == 4:
+    print(liczba1, "/", liczba2, "=", liczba1 / liczba2)
+
+else:
+    print("nieobsugiwana funkcja")
+
