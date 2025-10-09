6044508970c0882a2599f66242fb23728cf65ab3
Mateusz Kogut <mateuszkog@gmail.com>
2025-10-09 10:24:57 +0200

    Initial commit

---
+++ zadanieKtoreMiMamaKazala.py
@@ -0,0 +1,14 @@
+print("dobra miejmy to już za sobą...") 
+print("obliczę ci średnią ocen z 5 przedmoitów")
+
+a = int(input("podaj 1 ocenę: "))
+b = int(input("podaj 2 ocenę: "))
+c = int(input("podaj 3 ocenę: "))
+d = int(input("podaj 4 ocenę: "))
+e = int(input("podaj 5 ocenę: "))
+
+lacznaLiczba = a + b + c + d + e
+
+srednia = lacznaLiczba / 5
+
+print("oto średnia: ", srednia)
