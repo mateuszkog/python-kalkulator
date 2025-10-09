97f6d1644e7ae36efa5318f96caee562c3c2461c
Mateusz Kogut <mateuszkog@gmail.com>
2025-10-09 10:31:21 +0200

    another action added

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,6 @@
 liczba1 = float(input("podaj 1 liczbę: "))
 liczba2 = float(input("podaj 2 liczbę: "))
-wybierz = int(input("wybierz działanie (1 = +) (2 = -) (3 = *) (4 = /): "))
+wybierz = int(input("wybierz działanie (1 = +) (2 = -) (3 = *) (4 = /) (5 = **): "))
 
 if wybierz == 1:
     print(liczba1, "+", liczba2, "=", liczba1 + liczba2)
@@ -14,8 +14,11 @@ elif wybierz == 3:
 elif wybierz == 4:
     print(liczba1, "/", liczba2, "=", liczba1 / liczba2)
 
+elif wybierz == 5:
+    print(liczba1, "**", liczba2, "=", liczba1 ** liczba2)
+
 else:
-    print("miałeś wybrać w działaniu od 1 do 4 idioto")
+    print("miałeś wybrać w działaniu od 1 do 5 idioto")
 
 print("teraz należą mi się 3 franki szwajcarskie")
 print("możesz blikiem")
